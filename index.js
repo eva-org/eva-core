51bb90e4ed7d27f50109636b24fc963f45573a4a
gucong <gucong@raiyee.com>
2018-04-10 14:25:25 +0800

    commit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const electron = require('electron')
 // Module to control application life.
-const app = electron.app
+const {
+  app,
+  globalShortcut,
+  ipcMain
+} = electron
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
@@ -10,10 +14,15 @@ const url = require('url')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let isShow = false
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 500, height: 76, frame: false})
+  mainWindow = new BrowserWindow({
+    width: 500,
+    height: 76,
+    frame: false
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -26,7 +35,7 @@ function createWindow () {
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -34,13 +43,26 @@ function createWindow () {
   })
 }
 
+ipcMain.on('box-input', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.returnValue = 'pong'
+})
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow()
+  isShow = true
+  globalShortcut.register('CommandOrControl+Shift+M', () => {
+    console.log(isShow)
+    isShow ? mainWindow.hide() : mainWindow.show()
+    isShow = !isShow
+  })
+})
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', function() {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -48,7 +70,7 @@ app.on('window-all-closed', function () {
   }
 })
 
-app.on('activate', function () {
+app.on('activate', function() {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
