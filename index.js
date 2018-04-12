b074b96bb96f97f9ea9fd4f39c767685504a3b5c
gucong <gucong@raiyee.com>
2018-04-12 10:44:03 +0800

    update

--- index.js
+++ index.js
@@ -1,90 +1,64 @@
-const electron = require('electron')
+// 全局绑定
+global.electron = require('electron')
+global.path = require('path')
+global.url = require('url')
+
 // Module to control application life.
 const {
-  app,
-  globalShortcut,
-  ipcMain,
-  BrowserWindow
-} = electron
-// Module to create native browser window.
-
-const path = require('path')
-const url = require('url')
-const mainFun = require('./main')
+  electron: {
+    app,
+    globalShortcut,
+    ipcMain
+  },
+  path,
+  url
+} = global
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-let isShow = false
+// global.mainWindow
+
 let input = ''
 let query = ''
 // TODO 组件加载器
 const SearchInBaidu = require('./base_plugin/SearchInBaidu')
 const HbmLog = require('./extend_plugin/HbmLog')
 
-function createWindow(x, y) {
-
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    alwaysOnTop:true,
-    x: +x,
-    y: +y,
-    width: 500,
-    height: 76,
-    frame: false
-  })
-  console.log(mainWindow)
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-}
-
-ipcMain.on('box-input', (event, arg) => {
-  // TODO 实时校验系统
-  console.log(arg) // prints "ping"
-})
-
-ipcMain.on('box-input-enter', (event, arg) => {
-  // TODO 组件调用器
-  const inputArr = arg.split(' ')
-  if (inputArr[0] === 'bd') {
-    SearchInBaidu({
-      query: inputArr[1]
-    })
-    mainWindow.hide()
-  } else if (inputArr[0] === 'log') {
-    HbmLog({
-      query: inputArr[1]
-    })
-  }
-})
+// Module to create native browser window.
+const {createMainWindow} = require('./loaders/windowLoader')
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', () => {
-  const winW = electron.screen.getPrimaryDisplay().workAreaSize.width
-  const winH = electron.screen.getPrimaryDisplay().workAreaSize.height
-  const x = (winW / 2 - 250).toFixed(0)
-  const y = 90
-  createWindow(x, 90)
+  let isShow = false
+  global.mainWindow  = createMainWindow()
+  const {mainWindow} = global
+
   isShow = true
   globalShortcut.register('CommandOrControl+Shift+M', () => {
-    isShow ? mainWindow.hide() : mainWindow.showInactive()
+    isShow ? mainWindow.hide() : mainWindow.show()
     isShow = !isShow
+  })
+
+  ipcMain.on('box-input', (event, arg) => {
+    // TODO 实时校验系统
+    console.log(arg) // prints "ping"
+  })
+
+  ipcMain.on('box-input-enter', (event, arg) => {
+    // TODO 组件调用器
+    const inputArr = arg.split(' ')
+    if (inputArr[0] === 'bd') {
+      SearchInBaidu({
+        query: inputArr[1]
+      })
+      mainWindow.hide()
+      isShow = !isShow
+    } else if (inputArr[0] === 'log') {
+      HbmLog({
+        query: inputArr[1]
+      })
+    }
   })
 })
 
@@ -100,9 +74,7 @@ app.on('window-all-closed', function() {
 app.on('activate', function() {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    const winW = electron.screen.getPrimaryDisplay().workAreaSize.width
-    const winH = electron.screen.getPrimaryDisplay().workAreaSize.height
-    createWindow((winW / 2 - 250).toFixed(0), 90)
+  if (global.mainWindow === null) {
+    createMainWindow()
   }
 })
---
+++ loaders/WindowLoader/index.js
@@ -0,0 +1,38 @@
+module.exports = {
+  createMainWindow: () => {
+    // Create the browser window.
+    const {electron:{
+      BrowserWindow
+    }} = global
+    const x = (electron.screen.getPrimaryDisplay().workAreaSize.width / 2 - 250).toFixed(0)
+    const y = 90
+
+    let mainWindow = new BrowserWindow({
+      alwaysOnTop: true,
+      x: +x,
+      y: +y,
+      width: 500,
+      height: 76,
+      frame: false
+    })
+    
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+      pathname: path.join(__dirname, 'views/main.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
+
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      // mainWindow = null
+    })
+    return mainWindow
+  }
+}
