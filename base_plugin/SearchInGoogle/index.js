cd067658a9e78aef3eb38c701e3e40d95e11eda5
ziyuan <hanziyuan08@outlook.com>
2018-04-16 16:48:24 +0800

    google search

--- base_plugin/SearchInGoogle/index.js
+++ base_plugin/SearchInGoogle/index.js
@@ -1,20 +1,25 @@
+const child_process = require('child_process');
+
 module.exports = {
-  name: 'SearchInGoogle',
-  quick: 'g',
-  exec: ({query}) => {
-    const child_process = require('child_process');
-
-    const openUrl = `https://www.google.com/search?q=${query}`
-    let cmd
-
-    if (process.platform == 'win32') {
-      cmd = 'start'
-      return child_process.exec(`${cmd} ${openUrl}`)
-    } else if (process.platform == 'linux') {
-      cmd = 'xdg-open'
-    } else if (process.platform == 'darwin') {
-      cmd = 'open'
-    }
-    child_process.exec(`${cmd} "${openUrl}"`)
+  name: 'SearchInBaidu',
+  quick: 'bd',
+  query: ({param}) => {
+    return [{
+      title: '百度搜索',
+      subTitle: '基于百度搜索',
+      action() {
+        const openUrl = `https://www.google.com/search?q=${query}`
+        let cmd
+        if (process.platform === 'win32') {
+          cmd = 'start'
+          return child_process.exec(`${cmd} ${openUrl}`)
+        } else if (process.platform === 'linux') {
+          cmd = 'xdg-open'
+        } else if (process.platform === 'darwin') {
+          cmd = 'open'
+        }
+        child_process.exec(`${cmd} "${openUrl}"`)
+      }
+    }]
   }
 }
