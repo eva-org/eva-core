542e47dec5a15bdb8cd91def02900661a8e97f2d
ziyuan <onbpazy@163.com>
2018-04-20 00:04:52 +0800

    `Find App` now support windows and mac, but still not support linux

--- base_plugin/FindApp/index.js
+++ base_plugin/FindApp/index.js
@@ -1,25 +1,51 @@
 const child_process = require('child_process');
 const glob = require('glob')
+
 let files = []
-glob('/Applications/**.app', (err, file) => {
-  files = file.toString().split(',')
-})
-const buildLine = (title, subTitle = '', action = () => {
-}) => {
-  return {
-    title,
-    subTitle,
-    action
-  }
+let pattern
+let filePrefix
+let command
+
+let initialized = false
+
+function initAndGetData(pluginContext) {
+  const {utils: {isMac, isWindows, isLinux, logger}} = pluginContext
+  return new Promise(resolve => {
+    if (isMac) {
+      pattern = '/Applications/**.app'
+      filePrefix = '/Applications/'
+      command = 'open '
+    }
+    else if (isWindows) {
+      pattern = 'C:/ProgramData/Microsoft/Windows/Start Menu/Programs/**.lnk'
+      filePrefix = 'C:/ProgramData/Microsoft/Windows/Start Menu/Programs/'
+      command = ''
+    } else if (isLinux) {
+      // TODO linux support
+    } else {
+      logger.error('Not support current system.')
+    }
+
+    glob(pattern, (err, file) => {
+      files = file.toString().split(',')
+      initialized = true
+      resolve(getData(pluginContext))
+    })
+  })
 }
+
 const getData = ({query}) => {
   return new Promise(resolve => {
-    const resultFileArr = files.filter(item => item.replace('/Applications/', '').toUpperCase().indexOf(query.toUpperCase()) >= 0)
+    const resultFileArr = files.filter(item => item.replace(filePrefix, '').toUpperCase().indexOf(query.toUpperCase()) >= 0)
     const resultArr = []
     resultFileArr.forEach(item => {
-      resultArr.push(buildLine(item.replace('/Applications/', ''), `打开 ${item}`, () => {
-        child_process.exec(`open "${item}"`)
-      }))
+      resultArr.push({
+        title: item.replace(filePrefix, ''),
+        subTitle: `打开 ${item}`,
+        action() {
+          child_process.exec(`${command}"${item}"`)
+        }
+      })
     })
     resolve(resultArr)
   })
@@ -28,7 +54,8 @@ const getData = ({query}) => {
 module.exports = {
   name: 'FindApp',
   quick: 'op',
-  async query (pluginContext) {
+  async query(pluginContext) {
+    if (!initialized) return initAndGetData(pluginContext)
     return getData(pluginContext)
   }
 }
