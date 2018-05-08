83fccd5124b60056c2a9e3fa675557da34533b66
ziyuan <hanziyuan08@outlook.com>
2018-05-08 09:19:28 +0800

    删除多余的括号

--- app/base_plugin/FindApp/index.js
+++ app/base_plugin/FindApp/index.js
@@ -42,7 +42,7 @@ async function initAndGetData(pluginContext) {
 
   for (const pattern of config.patterns) {
     await glob.promise(pattern, (err, file) => {
-      files = files.concat((file.toString().split(',').filter(() => true)))
+      files = files.concat(file.toString().split(',').filter(() => true))
     })
   }
 
